d7e8fa99f0620c38b7be9e0c29234cee3048fd6b
zhonghaoxi <zhonghaoxi@yeah.net>
2018-06-17 23:21:35 +0800

    push

--- result.py
+++ result.py
@@ -9,40 +9,36 @@ args = parser.parse_args()
 path = args.path
 name = path.split("/")[-1]
 
-result = {
-    "crit": {"acc": 0, "mp": 0, "mr": 0, "f1": 0},
-    "law": {"acc": 0, "mp": 0, "mr": 0, "f1": 0},
-    "time": {"acc": 0, "mp": 0, "mr": 0, "f1": 0}
-}
+key_list = ["mi_pre", "ma_pre", "mi_rec", "ma_rec", "mi_f1", "ma_f1", "cnt"]
+task_list = ["crit", "law", "time"]
 
-map_list = {
-    0: "acc",
-    1: "mp",
-    2: "acc",
-    3: "mr",
-    4: "acc",
-    5: "f1"
-}
+result = {}
+
+for a in task_list:
+    result[a] = {}
+    for b in key_list:
+        result[a][b] = 0
 
 cnt = 0
 try:
     while True:
         cnt += 1
-        for task in result.keys():
+        for task in task_list:
             f = open(os.path.join(path, "%d-%s" % (cnt, task)), "r")
             nowv = {}
             for a in range(0, 6):
                 value = float(f.readline()[:-1].split("\t")[-1])
-                nowv[map_list[a]] = value
-            if nowv["f1"] > result[task]["f1"]:
+                nowv[key_list[a]] = value
+            if nowv["mi_f1"] + nowv["ma_f1"] > result[task]["mi_f1"] + result[task]["ma_f1"]:
                 for x in nowv.keys():
                     result[task][x] = nowv[x]
+                result[task]["cnt"] = cnt
 except Exception as e:
     print(e)
 
 f = open("result/%s" % name, "w")
-for x in ["law","crit","time"]:
-    print(x,[result[x]["acc"],result[x]["mp"],result[x]["mr"],result[x]["f1"]], file=f)
-    print(x,[result[x]["acc"],result[x]["mp"],result[x]["mr"],result[x]["f1"]])
+for x in ["law", "crit", "time"]:
+    print(x, json.dumps(result[x]), file=f)
+    print(x, json.dumps(result[x]))
 
 f.close()
