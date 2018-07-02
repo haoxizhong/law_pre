f21a1ece543374be74c11377c45052ef7e1b340f
zhonghaoxi <zhonghaoxi@yeah.net>
2018-07-02 21:13:55 +0800

    gg

--- word2vec/transformer.py
+++ word2vec/transformer.py
@@ -7,13 +7,16 @@ out_path = "/data/disk1/private/zhx/law/data/big_new"
 for file_name in os.listdir(path):
     f = open(os.path.join(path, file_name), "r")
     ouf = open(os.path.join(out_path, file_name), "w")
+    print(file_name)
 
     for line in f:
-        arr = []
-        data = json.loads(line)
-        for y in data["fact"]:
-            arr.append([x for x in y if x != "\r" and x != "\n"])
-        arr = [x for x in arr if x != []]
+        try:
+            arr = []
+            data = json.loads(line)
+            for y in data["fact"]:
+                arr.append([x for x in y if x != "\r" and x != "\n"])
+            arr = [x for x in arr if x != []]
 
-        print(json.dumps({"fact": arr, "meta": data["meta"]},ensure_ascii=False),file=ouf)
-
+            print(json.dumps({"fact": arr, "meta": data["meta"]}, ensure_ascii=False), file=ouf)
+        except Exception as e:
+            print(line[:-1])
