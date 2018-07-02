85d8a8ee41322ed6b23836c2f4a47a3e1c5ab672
zhonghaoxi <zhonghaoxi@yeah.net>
2018-07-02 21:07:18 +0800

    check

--- word2vec/transformer.py
+++ word2vec/transformer.py
@@ -2,20 +2,20 @@ import os
 import json
 
 path = "/data/disk1/private/zhx/law/data/big"
-out_path = "/data/dis1k/private/zhx/law/data/one/data"
-
-ouf = open(out_path, "w")
+out_path = "/data/dis1k/private/zhx/law/data/big_new"
 
 for file_name in os.listdir(path):
     f = open(os.path.join(path, file_name), "r")
+    ouf = open(os.path.join(out_path, file_name), "w")
 
     for line in f:
         arr = []
         data = json.loads(line)
-        for x in data["fact"]:
-            arr.append(" ".join(x))
-        result = " 。 ".join(arr)
-        print(result, file=ouf)
+        for y in data["fact"]:
+            arr.append([x != "\r" and x != "\n" for x in y])
+        arr = [x != [] for x in arr]
+
+        print(json.dumps({"fact": arr, "meta": data["meta"]}))
 
         break
     break
